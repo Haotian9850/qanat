229f72f58a6416727773a5c072b819158bf88bc4
Haotian9850 <haotian9850@gmail.com>
2019-10-01 17:38:34 -0400

    added schema

---
+++ tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS vehicle(
+    car_id INT NOT NULL AUTO_INCREMENT,
+    vin BIGINT NOT NULL,
+    make VARCHAR(255) NOT NULL,
+    model VARCHAR(255) NOT NULL 
+    
+)
