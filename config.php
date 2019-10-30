3ade0f1fe94e3f42082b9265aa2cd4158ffe0681
Haotian9850 <haotian9850@gmail.com>
2019-10-30 17:37:49 -0400

    added config script

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+  ini_set("display_errors", true); 
+  date_default_timezone_set("America/New_York");  
+
+  define("SERVER", "localhost");
+  define("USERNAME", "root");
+  define("PASSWORD", "");   // YOUR SQL PASSWORD
+  define("DATABASE", "qanat");
+
+?>
