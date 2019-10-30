8d3c9b1433aac2e6d28a8600d24ce43e6bd66e18
Haotian9850 <haotian9850@gmail.com>
2019-10-30 17:45:59 -0400

    added connection script

---
+++ connect.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+    require("config.php");
+    
+    function get_sql_connection(){
+        $conn =  new mysqli(
+            SERVER,
+            USERNAME,
+            PASSWORD,
+            DATABASE
+        );
+        if($conn->connect_errno){
+            printf("Connection to SQL server %s failed: %s\n", SERVER, $conn->connect_error);
+            exit();
+        }
+        if($conn->ping()){
+            printf("Connection to SQL server %s is successful. \n", SERVER);
+        }else{
+            printf("SQL error: %s", $conn->error);
+        }
+        return $conn;
+    }
+
+
+
+
+
+?>
