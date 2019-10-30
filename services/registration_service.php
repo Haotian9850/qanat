f2f878dcd4b28306ef3ca7fd6b181aedaba361ce
Haotian9850 <haotian9850@gmail.com>
2019-10-30 19:00:06 -0400

    updated registration service PASSED TESTS

--- services/registration_service.php
+++ services/registration_service.php
@@ -10,12 +10,13 @@
      *  {
      *      VIN: str
      *      make: str
+     *      model: str
      *      year: int
      *  }
      */
     function register($username, $password, $cars){
-        register_user($username, $password);
-        register_cars($cars);
+        $conn = get_sql_connection();
+        register_cars($conn, $cars, register_user($conn, $username, $password));
     }
 
     /**
@@ -23,7 +24,7 @@
      *  user_id of last inserted user
      */
     function register_user($conn, $username, $password){
-        $create_user = $conn->prepare("INSERT INTO User (username, password) VALUES (?, ?");
+        $create_user = $conn->prepare("INSERT INTO User (username, password) VALUES (?, ?)");
         $create_user->bind_param(
             "ss",
             $username,
@@ -34,20 +35,35 @@
         }catch(Exception $e){
             echo $e->getMessage();
         }
-        return $mysqli->insert_id();
+        return mysqli_insert_id($conn);
     }
 
 
     function register_cars($conn, $cars, $user_id){
         foreach($cars as $car){
-            register_single_car($car);
+            register_single_car($conn, $car, $user_id);
         }
     }
 
 
     function register_single_car($conn, $car, $user_id){
+        $insert_car_type = $conn->prepare(
+            "INSERT INTO Car_Type (make, model, year) VALUES (?, ?, ?)"
+        );
+        $insert_car_type->bind_param(
+            "ssi",
+            $car->make,
+            $car->model,
+            $car->year
+        );
+        try{
+            $insert_car_type->execute();
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+
         $insert_car = $conn->prepare(
-            "INSERT INTO Vehicle (VIN, make, model, year) values (?, ?, ?, ?)"
+            "INSERT INTO Vehicle (VIN, make, model, year) VALUES (?, ?, ?, ?)"
         );
         $insert_car->bind_param(
             "sssi",
@@ -61,10 +77,10 @@
         }catch(Exception $e){
             echo $e->getMessage();
         }
+
         $insert_ownership = $conn->prepare(
             "INSERT INTO Owns (VIN, user_id) VALUES (?, ?)"
         );
-
         $insert_ownership->bind_param(
             "ss",
             $car->VIN,
@@ -76,5 +92,18 @@
             echo $e->getMessage();
         }
     }
+
+
+
+    /*
+    $arr = Array();
+    $arr[] = (object)array(
+        "VIN"=>"123456ASDDF",
+        "make"=>"Ford",
+        "model"=>"Fiesta",
+        "year"=>2019
+    );
+    register("haotian", "123456", $arr);
+    */
 
 ?>
