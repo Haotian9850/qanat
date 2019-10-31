1eeb32c221bfa5ab7d24f3df76ce8626cc627905
Haotian9850 <haotian9850@gmail.com>
2019-10-31 15:46:13 -0400

    updated station service PASSED TESTS

--- services/station_service.php
+++ services/station_service.php
@@ -12,6 +12,7 @@
      *  result: a nested list of station objects
      *  [{
      *      station_id: int,
+     *      num_plugs: int,
      *      location: str,
      *      plugs: [{
      *                  model_no: str,
@@ -19,7 +20,7 @@
      *              },]
      *  },]  
      */
-    function get_all_stations(){
+    function get_all_stations($conn){
         $conn = get_sql_connection();
         $result = Array();
         $all_stations = "SELECT * FROM Station";
@@ -29,10 +30,33 @@
             echo $e->getMessage();
         }
         while($row = $stations->fetch_assoc()){
-            var_dump($row);
+            $result[] = get_plugs_for_station($row["station_ID"], $conn);
         }
+        var_dump($result);
     }
 
-    get_all_stations();
+    /**
+     * @Returns
+     *  result: list of plugs
+     *  [{
+     *      model_no: str,
+     *      charge_speed: int
+     *  },]
+     */
+    function get_plugs_for_station($station_id, $conn){
+        $result = Array();
+        $find_plugs = "SELECT DISTINCT p.model_no, q.serial_no, charge_speed FROM Hosts NATURAL JOIN Station NATURAL JOIN Plug_Model p NATURAL JOIN Plug q WHERE station_ID = '$station_id'";
+        try{
+            $plugs = mysqli_query($conn, $find_plugs);
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+        while($row = $plugs->fetch_assoc()){
+            $result[] = $row;
+        }
+        return $result;
+    }
+
+    get_all_stations(get_sql_connection());
 
 ?>
