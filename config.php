af188fc72e6238605d25d0ff52222931bf599147
Sean Gatewood <stg2bd@virginia.edu>
2019-11-13 13:07:03 -0500

    Add Neil's config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+  ini_set("display_errors", true);
+  date_default_timezone_set("America/New_York");
+
+  define("SERVER", "cs4750.cs.virginia.edu");    //YOUR SQL HOSTNAME
+  define("USERNAME", "ns9wr");   //YOUR SQL USERNAME
+  define("PASSWORD", "upsorn");   // YOUR SQL PASSWORD
+  define("DATABASE", "ns9wr_qanat");
+
+  define("TEMPLATE_PATH", "templates/");
+  define("SERVICE_PATH", "services/");
+
+?>
