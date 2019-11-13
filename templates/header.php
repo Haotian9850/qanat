9bb24e9e4ac8e87a7a94a832904d2c2b79748ad4
Sean Gatewood <stg2bd@virginia.edu>
2019-11-13 13:09:12 -0500

    Fix style.css path

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="style.css">
 
     <title>Qanat</title>
   </head>
@@ -21,7 +21,7 @@
           </div>
         <?php } ?>
         &nbsp;&nbsp;&nbsp;
-        
+
         &nbsp;&nbsp;&nbsp;
         <?php if(!isset($_SESSION["username"])){ ?>
           <a href="?action=register" class="btn btn-light">Register</a>
