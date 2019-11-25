dfc0ae37c458a658ac31753052ba34a7f072afd6
Neil Singh <ns9wr@virginia.edu>
2019-11-25 18:08:38 -0500

    updated to actually work

--- services/review_service.php
+++ services/review_service.php
@@ -1,19 +1,27 @@
 <?php
     
-        include_once("./library.php");
+        	include_once("./library.php");
 		// To connect to the database
 		$con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 		// Check connection
 		if (mysqli_connect_errno())
 		{echo "Failed to connect to MySQL: " . mysqli_connect_error();}
 		
+
+
 		// Form the SQL query (an INSERT query)
 		
-		$sql="INSERT INTO Persons (?, ?, ?) VALUES(NULL,'$_POST[inputtext]','$_POST[inputrate]')";
+		$sql="INSERT INTO Review (text, rating) VALUES('$_POST[inputtext]','$_POST[inputrate]')";
 		
 		if (!mysqli_query($con,$sql))
 		{die('Error: ' . mysqli_error($con));}
-		echo "1 record added";
+		//Citation: consulted this Stack Overflow link to learn how to refresh page after a submission
+		//https://stackoverflow.com/questions/42109893/how-to-go-back-to-page-after-submitting-form-using-php
+		header("Location: reviews.php");
+		echo "WooHoo";
+
+
+
 		// Output to user
 		mysqli_close($con);
 ?>
--- templates/reviews.php
+++ templates/reviews.php
@@ -5,32 +5,58 @@
 <div class="main-content">
     <div class="main-block">
         <div class="row row-margined">
-            <h1 class="display-4">Review</h1>
+            <h1 class="display-4">Review Portal</h1>
         </div>
         <div class="row row-margined">
-            <form action="?action=mkreview" method="POST">
-                <div class="form-row" id="carInput">
+            <form action="review_service.php" method="POST">
+           
                     <div class="form-group col-md-4">
                         <label>Comment Text</label>
-                        <input type="text" size="150" maxlength="95" class="form-control" name="reviewarr[]">
+                        <input type="text" size="150" maxlength="95" class="form-control" name="inputtext">
                     </div>
-					<div class="form-group col-md-4">
+		    <div class="form-group col-md-4">
                         <label>Rating (out of 100) </label>
-                        <input type="text" size="3" maxlength="3" class="form-control" name="ratingarr[]">
+                        <input type="text" size="3" maxlength="3" class="form-control" name="inputrate">
                     </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+             <button type="submit" class="btn btn-primary">Submit</button>
+	     
+ 	     
             </form>
-			
-			<label>Current Reviews</label>
-			<?php foreach($review["text"] as $rev){ ?>
-                  <li class="list-group-item d-flex justify-content-between align-items-center plug">
-                    <b>Text: </b><?php echo $rev["text"] ?>
-                    <b>Rating: </b><?php echo $rev["rating"] ?>
-                  </li>
-                <?php } ?>
-                
-			
+
+	<div class="form-group col-md-4">
+            <h1 class="display-4">Ratings</h1>
+
+	
+	    <?php
+
+		require_once('./library.php');
+
+		$con= new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+
+		// Check connection
+
+		if (mysqli_connect_errno()) 
+		{
+			echo("Can't connect to MySQL Server Segmentation Fault. Error code: " . mysqli_connect_error());
+			return null;
+		}
+
+		// Form the SQL query (a SELECT query)
+
+		$sql="SELECT * FROM Review";
+		$result = mysqli_query($con,$sql);
+	 	while($row = mysqli_fetch_array($result)){
+            		
+                	echo $row['text'];
+	                echo " " . $row['rating'];
+			echo "<br>";
+        	}
+		
+        	mysqli_close($con);
+
+	?>
+	</div>
         </div>
     </div>
 </div>
