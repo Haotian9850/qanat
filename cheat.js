0feb9708f3ee295698bef660ac4b6e3c27974b2f
Sean Gatewood <stg2bd@virginia.edu>
2019-11-28 02:06:46 -0500

    Compatible plugs

--- cheat.js
+++ cheat.js
@@ -1,36 +1,95 @@
 $(function(){
 
-	$(".list-group").each(function(){
-		var plugs = $(this).children("li");
-		plugs.each(function(count){
-			if(count >= 5){
-				$(this).addClass("d-none");
-				$(this).removeClass("d-flex");
-			}
+	function hide_most(){
+		$(".list-group").each(function(){
+			var plugs = $(this).children("li");
+			var count = 0;
+			plugs.each(function(){
+				if($(this).hasClass("d-flex")){
+					count++;
+				}
+				if(count >= 5){
+					$(this).addClass("d-none");
+					$(this).removeClass("d-flex");
+				}
+			});
 		});
-	});
+	}
+
+	hide_most();
+	
+
+	var filtered = false;
+	var hidden = true;
+
+	function hide_bs_element(el){
+		el.addClass("d-none");
+		el.removeClass("d-flex");
+	}
+
+	function show_bs_element(el){
+		el.removeClass("d-none");
+		el.addClass("d-flex");
+	}
 
 	$(".show-all").click(function(){
 		var plugs = $(this).prev().children("li");
 
 		if(!$(this).prop("shown") || $(this).prop("shown") == "false"){
+			hidden = false;
 			plugs.each(function(){
-				$(this).removeClass("d-none");
-				$(this).addClass("d-flex");
+				if(!filtered || ($(this).attr("compatible") == "true")){
+					show_bs_element($(this))
+				} 
 			});
 			$(this).text("Hide most");
 			$(this).prop("shown","true");
 		}else{
-			plugs.each(function(count){
+			hidden = true;
+			var count = 0;
+			plugs.each(function(){
+				if($(this).hasClass("d-flex")){
+					count++;
+				}
 				if(count >= 5){
-					$(this).addClass("d-none");
-					$(this).removeClass("d-flex");
+					hide_bs_element($(this));
 				}
 			});
 			$(this).text("Show all");
 			$(this).prop("shown","false");
 		}
 		
+	});
+
+	$("#filter").click(function(e){
+		var box = $(this).children("input");
+		if(box.prop("checked")){
+			$(".total_plug_ct").hide();
+			$(".compatible_plug_ct").show();
+			filtered = true;
+			$(this).css("font-weight","bold");
+			$(".list-group").children("li").each(function(){
+				show_bs_element($(this));
+			});
+			var incompatible = $(".list-group li[compatible=false]");
+			incompatible.each(function(){
+				hide_bs_element($(this));
+			});
+			if(hidden){
+				hide_most();
+			}
+		}else{
+			filtered = false;
+			$(".total_plug_ct").show();
+			$(".compatible_plug_ct").hide();
+			$(this).css("font-weight","");
+			$(".list-group").children("li").each(function(){
+				show_bs_element($(this));
+			});
+			if(hidden){
+				hide_most();
+			}
+		}
 	});
 
 });
--- templates/homepage.php
+++ templates/homepage.php
@@ -9,7 +9,10 @@
             <h1 class="display-4">Qanat: Find Charging for Your Electric Car</h1>
         </div>
         <div class="row row-margined">
-          <label><input type="checkbox"> <strong>Only show plugs that my car can use</strong></label>
+          <?php if(strlen($_SESSION["username"]) > 0){?>
+            <label id="filter"><input type="checkbox"> <strong>Only show plugs that my car can use</strong></label>
+          <?php } ?>
+          
           <h2 class="display-5">Here are a list of charging stations available in our database: </h2>
         </div>
         <div class="row row-margined">
@@ -19,13 +22,11 @@
                 <?php echo $station["location"] ?>
               </div>
               <div class="card-body">
-                <h5 class="card-title"><?php echo $station["num_plugs"] ?> plugs in total</h5>
+                <h5 class="card-title total_plug_ct"><?php echo $station["num_plugs"] ?> plugs in total</h5>
+                <h5 class="card-title compatible_plug_ct" style="display: none;"><?php echo $station["num_compatible"] ?> compatible plugs</h5>
                 <ul class="list-group">
-                  <?php
-                    echo count($station["plugs"]);
-                  ?>
                 <?php foreach($station["plugs"] as $plug){ ?>
-                  <li class="list-group-item d-flex justify-content-between align-items-center plug">
+                  <li class="list-group-item d-flex justify-content-between align-items-center plug" compatible=<?php echo $plug["compatible"]?> >
                     <b>Model: </b><?php echo $plug["model_no"] ?>
                     <b>Serial: </b><?php echo $plug["serial_no"] ?>
                     <span class="badge badge-primary badge-pill">
